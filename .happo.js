a2c13fe4795ee867f38207bdb4730a4cbb20a35c
Parris Khachi <parris@letswoosh.com>
2017-12-10 17:22:08 -0800

    Happo Classic Runner w/ Next.js

---
+++ .happo.js
@@ -0,0 +1,30 @@
+// IMPORTANT: This is definition is for happo classic
+
+const FirefoxTarget = require('happo-target-firefox');
+const S3Uploader = require('happo-uploader-s3');
+
+module.exports = {
+  // the name of the summary file, it gets placed into the screenshots folder
+  // (default is shown below)
+  resultSummaryFilename: 'resultSummary.json',
+
+  // A function that returns an "Uploader" interface for CI.
+  // (default: null)
+  // uploader: () => new S3Uploader(),
+
+  // Specify the folder where snapshots are saved
+  // (default: 'snapshots')
+  snapshotsFolder: 'happoClassic/screenshots',
+
+  // An array of "targets" to run. Targets specify the environment to run
+  // the snapshots in. Must specify at least one.
+  // (default: [])
+  targets: [
+    new FirefoxTarget({
+      // ... configuration for FirefoxTarget
+      sourceFiles: [
+        'happoClassic/dist/main.js',
+      ]
+    }),
+  ],
+};
